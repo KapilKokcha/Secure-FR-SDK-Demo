ffc1b27fa4c145930efc0eb5608474f6a0e368f0
kapil <kapilkokcha.k@gmail.com>
2025-04-26 23:06:56 +0530

    Demo App for testing the secure-fr-sdk

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import FaceAuth from './FaceAuth'; // Adjust the path if needed
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Face Authentication Demo</h1>
+        <FaceAuth />
+      </header>
+    </div>
+  );
+}
+
+export default App;
